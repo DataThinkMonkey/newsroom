489233b776e3e0e91fcbd65ad7d3cd80f5730658
jared bernard <jared@alma.local>
2018-02-09 23:01:31 -0600

    Modified sources.

--- src/sources.js
+++ src/sources.js
@@ -1,20 +1,35 @@
 module.exports = {
-  inside: {
-    rss: 'http://feeds.feedburner.com/inside-blog-taiwan',
-  },
-  bnext: {
-    rss: 'https://www.bnext.com.tw/rss',
-  },
-  nodeweekly: {
-    rss: 'https://nodeweekly.com/rss/',
-  },
-  wanqu: {
-    rss: 'https://wanqu.co/feed',
-  },
   techcrunch: {
     rss: 'http://feeds.feedburner.com/TechCrunch/',
   },
-  codetengu: {
-    rss: 'https://weekly.codetengu.com/issues.rss',
+  hackernews: {
+    rss: 'https://news.ycombinator.com/rss',
+  },
+  drudge: {
+    rss: 'http://www.drudgereportfeed.com/rss',
+  },
+  distrowatch: {
+    rss: 'https://distrowatch.com/news/dw.xml',
+  },
+  lifehacker: {
+    rss: 'https://lifehacker.com/rss',
+  },
+  cnet: {
+    rss: 'https://www.cnet.com/rss/news/',
+  },
+  fox: {
+    rss: 'http://feeds.foxnews.com/foxnews/latest',
+  },
+  linux: {
+    rss: 'https://feedproxy.google.com/linuxtoday/linux',
+  },
+  dodgers: {
+    rss: 'http://feeds.feedburner.com/dailydodgers-news',
+  },
+  ldsnews: {
+    rss: 'http://feeds.lds.org/LDSNewsRoomTop15',
+  },
+  ldsorg: {
+    rss: 'http://feeds.lds.org/ldsHomeFeatures',
   },
 };
